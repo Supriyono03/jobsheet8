e5f965e086efb6925e81f5353ce963d7b0eed5dc
Supriyono03 <dindadinara3@email.com>
2024-06-11 15:50:58 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: SafeArea(
+        child: Wrap(
+          children: [
+            buatKotak(Colors.greenAccent, 100),
+            buatKotak(Colors.orangeAccent[400]!, 70),
+            buatKotak(Colors.greenAccent, 50),
+            buatKotak(Colors.orangeAccent[400]!, 90),
+            buatKotak(Colors.red[300]!, 110),
+            buatKotak(Colors.blue[300]!, 30),
+          ],
+        ),
+      ),
+    ),
+  ));
+}
+
+Widget buatKotak(Color warna, double ukuran) {
+  return Container(
+    color: warna,
+    width: ukuran,
+    height: ukuran,
+    margin: EdgeInsets.all(4.0),
+  );
+}
